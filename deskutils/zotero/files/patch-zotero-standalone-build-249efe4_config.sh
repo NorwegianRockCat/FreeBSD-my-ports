32a213a41148619981430c97661533b78f67b65d
Trenton Schulz <trenton@norwegianrockcat.com>
2020-07-23 08:51:34 +0200

    Update Zotero to 5.0.89

--- deskutils/zotero/files/patch-zotero-standalone-build-249efe4_config.sh
+++ deskutils/zotero/files/patch-zotero-standalone-build-249efe4_config.sh
@@ -1,10 +1,10 @@
---- zotero-standalone-build-90a781b/config.sh.orig	2019-06-21 10:32:41 UTC
-+++ zotero-standalone-build-90a781b/config.sh
+--- zotero-standalone-build-249efe4/config.sh.orig	2020-07-08 09:59:53 UTC
++++ zotero-standalone-build-249efe4/config.sh
 @@ -10,6 +10,7 @@ MAC_RUNTIME_PATH="$DIR/xulrunner/Firefox.app"
  WIN32_RUNTIME_PATH="$DIR/xulrunner/firefox-win32"
  LINUX_i686_RUNTIME_PATH="$DIR/xulrunner/firefox-i686"
  LINUX_x86_64_RUNTIME_PATH="$DIR/xulrunner/firefox-x86_64"
 +FREEBSD_amd64_RUNTIME_PATH="$DIR/xulrunner/firefox-amd64"
-
+ 
  PDF_TOOLS_VERSION="0.0.3"
  PDF_TOOLS_URL="https://zotero-download.s3.amazonaws.com/pdftools/pdftools-$PDF_TOOLS_VERSION.tar.gz"
--- deskutils/zotero/files/patch-zotero-standalone-build-249efe4_fetch__xulrunner.sh
+++ deskutils/zotero/files/patch-zotero-standalone-build-249efe4_fetch__xulrunner.sh
@@ -1,12 +1,12 @@
---- zotero-standalone-build-90a781b/fetch_xulrunner.sh.orig	2019-12-02 00:27:47.000000000 +0100
-+++ zotero-standalone-build-90a781b/fetch_xulrunner.sh	2019-12-10 20:55:29.936017000 +0100
+--- zotero-standalone-build-249efe4/fetch_xulrunner.sh.orig	2020-07-08 09:59:53 UTC
++++ zotero-standalone-build-249efe4/fetch_xulrunner.sh
 @@ -1,4 +1,4 @@
 -#!/bin/bash
 +#!/usr/local/bin/bash
  set -euo pipefail
  
  # Copyright (c) 2011  Zotero
-@@ -26,7 +26,7 @@
+@@ -26,7 +26,7 @@ function usage {
  	cat >&2 <<DONE
  Usage: $0 -p platforms [-s]
  Options
@@ -15,7 +15,7 @@
   -s                  Skip download; Firefox must already be extracted in xulrunner/ directory
  DONE
  	exit 1
-@@ -35,6 +35,7 @@
+@@ -35,6 +35,7 @@ DONE
  BUILD_MAC=0
  BUILD_WIN32=0
  BUILD_LINUX=0
@@ -23,7 +23,7 @@
  skip_download=0
  while getopts "p:s" opt; do
  	case $opt in
-@@ -45,6 +46,7 @@
+@@ -45,6 +46,7 @@ while getopts "p:s" opt; do
  					m) BUILD_MAC=1;;
  					w) BUILD_WIN32=1;;
  					l) BUILD_LINUX=1;;
@@ -31,7 +31,7 @@
  					*)
  						echo "$0: Invalid platform option ${OPTARG:i:1}"
  						usage
-@@ -61,7 +63,7 @@
+@@ -61,7 +63,7 @@ while getopts "p:s" opt; do
  done
  
  # Require at least one platform
@@ -40,7 +40,7 @@
  	usage
  fi
  
-@@ -216,6 +218,24 @@
+@@ -219,6 +221,24 @@ if [ $BUILD_WIN32 == 1 ]; then
  	if [ $skip_download -eq 0 ]; then
  		rm "Firefox%20Setup%20$GECKO_VERSION.exe"
  	fi
@@ -65,10 +65,3 @@
  fi
  
  if [ $BUILD_LINUX == 1 ]; then
-@@ -255,4 +275,4 @@
- 	fi
- fi
- 
--echo Done
-\ No newline at end of file
-+echo Done
