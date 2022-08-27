ed201cadbb228d903e3858c1fe838caf33db381e
Trenton Schulz <trenton@norwegianrockcat.com>
2022-08-27 15:07:24 +0200

    Add additional patches

---
+++ deskutils/zotero/files/patch-zotero-6.0.11_scripts_pdf-reader.js
@@ -0,0 +1,11 @@
+--- zotero-6.0.11/scripts/pdf-reader.js.orig	2022-08-27 12:06:06 UTC
++++ zotero-6.0.11/scripts/pdf-reader.js
+@@ -36,7 +36,7 @@ async function getPDFReader(signatures) {
+ 			await fs.remove(path.join(targetDir, 'zotero'));
+ 		}
+ 		catch (e) {
+-			await exec('npm ci', { cwd: modulePath });
++			await exec('npm ci --legacy-peer-dep', { cwd: modulePath });
+ 			await exec('npm run build', { cwd: modulePath });
+ 			await fs.copy(path.join(modulePath, 'build', 'zotero'), targetDir);
+ 		}
---
+++ deskutils/zotero/files/patch-zotero-6.0.11_scripts_pdf-worker.js
@@ -0,0 +1,11 @@
+--- zotero-6.0.11/scripts/pdf-worker.js.orig	2022-08-27 12:07:39 UTC
++++ zotero-6.0.11/scripts/pdf-worker.js
+@@ -33,7 +33,7 @@ async function getPDFWorker(signatures) {
+ 			);
+ 		}
+ 		catch (e) {
+-			await exec('npm ci', { cwd: modulePath });
++			await exec('npm ci --legacy-peer-dep', { cwd: modulePath });
+ 			await exec('npm run build', { cwd: modulePath });
+ 			await fs.copy(path.join(modulePath, 'build', 'worker.js'), path.join(targetDir, 'worker.js'));
+ 		}
